3342ff122a74d72f7e54b168ac28a3a196348d03
Mark Antao <35659231+markantao@users.noreply.github.com>
2021-01-29 15:10:36 -0500

    Added a database

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template, redirect, flash, url_for, request, session
+from flask import Flask, render_template, redirect, flash, url_for, request, session, datetime
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlcehmy
 
 app = Flask(__name__)
 app.secret_key = "blogrr_project"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 app.permanent_session_lifetime = timedelta(weeks=52)
+
+db = SQLAlcehmy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(60), unique=True, nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', {self.email})'"
+
+Class Post(db.Model):
+    id = db.Column(db.Intefer, primary_key=True)
+    tite = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text(500), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}', '{self.content})"
+
+
 
 @app.route("/")
 def home():
